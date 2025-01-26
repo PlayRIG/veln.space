016c8b19dd7ab32a681c3018fefd2881900f9620
Rahxephonz <mynev.id@gmail.com>
2025-01-26 11:43:00 +0700

    adjust import files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 // @ts-check
-import { defineConfig } from "astro/config";
+import {defineConfig} from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import svgr from "vite-plugin-svgr";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({ nesting: true })],
+  integrations: [react(), tailwind({nesting: true})],
   experimental: {
     svg: true,
   },
@@ -32,6 +32,7 @@ export default defineConfig({
     resolve: {
       alias: {
         "@components": "/src/components",
+        "@layouts": "/src/layouts",
       },
     },
   },
